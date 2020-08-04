ce7e2c77c9d3feeb92f7e91f0334d6ad73db079e
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-08-04 15:24:47 +0300

    Updates

---
+++ precache-manifest.93096be79b8c994a56971f4c48268d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7962f0fea7a4323aa22ed8e9fb7f290",
+    "url": "/JetSolutions/index.html"
+  },
+  {
+    "revision": "5d347b5ab27cb7e45b67",
+    "url": "/JetSolutions/static/css/main.5587f87c.chunk.css"
+  },
+  {
+    "revision": "754b42af413541d7bf9d",
+    "url": "/JetSolutions/static/js/2.77321a0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JetSolutions/static/js/2.77321a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d347b5ab27cb7e45b67",
+    "url": "/JetSolutions/static/js/main.a546212b.chunk.js"
+  },
+  {
+    "revision": "0202c2bfd2a1b9c3b946",
+    "url": "/JetSolutions/static/js/runtime-main.f206c1a0.js"
+  }
+]);
